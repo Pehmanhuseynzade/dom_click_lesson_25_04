be924084745cb6b0bf035f45b7d7cb958f9efef1
ulvieyvazov <eyvazovulvi010101@gmail.com>
2023-04-25 19:45:37 +0400

    css html hamisi js de (25 aprel)

---
+++ jsdom/dom.js
@@ -0,0 +1,122 @@
+
+
+//TASK1:js dom ile yazin
+{/* <ul>
+<li>apple</li>
+<li>banana </li>
+<li>tomato</li>
+</ul>  */}
+
+// let ul = document.createElement("ul");
+
+// arr = ["banana","tomato","apple"]
+
+// arr.forEach(value => {
+//      li = document.createElement("li");
+//      li.textContent = value;
+//      ul.append(li)
+// });
+// document.body.appendChild(ul)
+
+//------------------------------------------------------------------------------
+
+//TASK2:ul de li lerin ilk son sozlerini deyishin
+
+// let ul  = document.querySelectorAll("ul")
+// console.log(ul)
+// ul.forEach(value => {
+//      let first = value.querySelector(`li:first-child`);
+//      let last = value.querySelector(`li:last-child`)
+//      first.textContent="first"
+//      last.textContent="last"
+// });
+
+//Task3:
+
+// console.log(document.body)
+// document.body.style.backgroundColor = "blue"
+
+// let section = document.createElement("section")
+
+// let result = document.createElement("div")
+// result.style.width = "40px"
+// result.style.height = "40px"
+// result.textContent = "0"
+// result.style.border = "1px solid black"
+// result.style.backgroundcolor = "white"
+// result.style.display = "flex"
+// result.style.justifyContent = "center"
+// result.style.alignItems =  "center"
+// result.style.margin = "0 auto"
+// result.style.rowGap = "20px"
+// result.style.color = "white"
+
+// document.body.appendChild(result)
+
+
+
+// section.style.display = "flex"
+// section.style.justifyContent = "center"
+// section.style.alignItems =  "center"
+// section.style.gap = "30px"
+// section.style.columnGap = "20px"
+
+// document.body.appendChild(section)
+
+
+
+// let btn1 = document.createElement("button")
+// btn1.style.width = "60px"
+// btn1.style.border = "1px solid black"
+// btn1.style.height = "40px"
+// btn1.style.backgroundColor = "black"
+// btn1.innerHTML = "-"
+// btn1.style.color = "white"
+
+// section.appendChild(btn1)
+
+// let btn2 = document.createElement("button")
+// btn2.style.width = "90px"
+// btn2.style.border = "1px solid black"
+// btn2.style.height = "40px"
+// btn2.style.backgroundColor = "black"
+// btn2.innerHTML = "Reset"
+// btn2.style.color = "white"
+
+// section.appendChild(btn2)
+
+// let btn3 = document.createElement("button")
+// btn3.style.width = "60px"
+// btn3.style.border = "1px solid black"
+// btn3.style.height = "40px"
+// btn3.style.backgroundColor = "black"
+// btn3.innerHTML = "+"
+// btn3.style.color = "white"
+
+// section.appendChild(btn3)
+// let count = 0
+
+// btn1.addEventListener("click",()=>{
+//      count-=1
+//      result.textContent = count
+// })
+
+// btn3.addEventListener("click",()=>{
+//      count+=1
+//      result.textContent = count
+// })
+
+// btn2.addEventListener("click",()=>{
+//      count = 0
+//      result.textContent = count
+// })
+
+
+
+
+
+//TASK4
+
+// console.log(document.body)
+// document.body.style.backgroundColor = "lightblue"
+
